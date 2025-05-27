37b9d353befb1c0c7e5f6e65c3a4e5b7726c90f0
Bounphithak <bounphithakphithak@yahoo.com>
2025-05-27 17:38:48 +0200

    restructuring the files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
 import { Service } from './entity/service.entity';
 
 @Module({
